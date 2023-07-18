c7d65113b56aabf5c1065741880d585fbc74a583
Sajeyks <sajeyksmwangi@gmail.com>
2023-07-18 13:47:14 +0300

    update: catch up with the changes

--- demo/demo/settings.py
+++ demo/demo/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'demo.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'default',
+        'USER': 'postgres',
+        'PASSWORD': 'pstSQL254',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
@@ -123,3 +127,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = ['{{EDUCATIVE_LIVE_VM_URL}}']
