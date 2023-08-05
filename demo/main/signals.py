8575f99eb11889f58a1b978f8f75d1af60f29c99
Sajeyks <sajeyksmwangi@gmail.com>
2023-08-05 09:18:40 +0300

    fix: comment main tenant addition

--- demo/main/signals.py
+++ demo/main/signals.py
@@ -1,22 +1,21 @@
-from django.db.models.signals import post_save, pre_delete, post_migrate
+from django.db.models.signals import post_save, pre_delete
 from django.dispatch import receiver
 from .models import Tenant
 import subprocess
-from django.conf import settings
 
 
-@receiver(post_migrate)
-def create_default_tenant(sender, **kwargs):
-    # Create the default 'main' tenant on startup if it doesn't exist
-    default_tenant, _ = Tenant.objects.get_or_create(name='main', subdomain_prefix='main')
-    default_tenant.save()
+# @receiver(post_migrate)
+# def create_default_tenant(sender, **kwargs):
+#     # Create the default 'main' tenant on startup if it doesn't exist
+#     default_tenant, _ = Tenant.objects.get_or_create(name='main', subdomain_prefix='main')
+#     default_tenant.save()
 
-    vm_url_without_https = '{{EDUCATIVE_LIVE_VM_URL}}'.replace('https://', '')
-    updated_hostname = f"main.{vm_url_without_https}"
+#     vm_url_without_https = '{{EDUCATIVE_LIVE_VM_URL}}'.replace('https://', '')
+#     updated_hostname = f"main.{vm_url_without_https}"
     
-    # Update /etc/hosts to replace 'localhost' with 'main'
-    update_host_command = f"echo '172.17.0.2 {updated_hostname}' | sudo tee -a /etc/hosts"
-    subprocess.run(update_host_command, shell=True)
+#     # Update /etc/hosts to replace 'localhost' with 'main'
+#     update_host_command = f"echo '172.17.0.2 {updated_hostname}' | sudo tee -a /etc/hosts"
+#     subprocess.run(update_host_command, shell=True)
 
 
 @receiver(post_save, sender=Tenant)
@@ -24,7 +23,7 @@ def add_tenant_to_hosts(sender, instance, created, **kwargs):
     if created or instance.subdomain_prefix == '':
         
         vm_url_without_https = '{{EDUCATIVE_LIVE_VM_URL}}'.replace('https://', '')
-        updated_hostname = f"{instance.subdomain_prefix}.main.{vm_url_without_https}"
+        updated_hostname = f"{instance.subdomain_prefix}.{vm_url_without_https}"
         
         # Update /etc/hosts to add the new tenant's domain or subdomain
         add_command = f"echo '172.17.0.2 {updated_hostname}' | sudo tee -a /etc/hosts"
@@ -36,7 +35,7 @@ def delete_tenant_from_hosts(sender, instance, **kwargs):
     if instance.subdomain_prefix == '':
         
         vm_url_without_https = '{{EDUCATIVE_LIVE_VM_URL}}'.replace('https://', '')
-        updated_hostname = f"{instance.subdomain_prefix}.main.{vm_url_without_https}"
+        updated_hostname = f"{instance.subdomain_prefix}.{vm_url_without_https}"
         
         # Update /etc/hosts to delete the tenant's domain or subdomain
         delete_command = f"sudo sed -i '/{updated_hostname}/d' /etc/hosts"
