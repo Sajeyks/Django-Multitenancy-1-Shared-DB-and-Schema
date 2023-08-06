9d7510fdb35a543cd891c0f4d5a50f9d3102e7a2
Sajeyks <sajeyksmwangi@gmail.com>
2023-08-06 16:44:51 +0300

    feat: separate tenant in admin

--- demo/main/admin.py
+++ demo/main/admin.py
@@ -1,9 +1,62 @@
 from django.contrib import admin
 from .models import customer, rocket, payload, launch, Tenant  # importing the models
+from .utils import tenant_from_request
 # Register your models here.
 
-admin.site.register(customer)
-admin.site.register(rocket)
-admin.site.register(payload)
-admin.site.register(launch)
+class customerAdmin(admin.ModelAdmin):
+    def get_queryset(self, request, *args, **kwargs):
+        queryset = super().get_queryset(request, *args, **kwargs)
+        tenant = tenant_from_request(request)
+        queryset = queryset.filter(tenant=tenant)
+        return queryset
+    
+    def save_model(self, request, obj, form, change):
+        tenant = tenant_from_request(request)
+        obj.tenant = tenant
+        super().save_model(request, obj, form, change)
+        
+
+class rocketAdmin(admin.ModelAdmin):
+    def get_queryset(self, request, *args, **kwargs):
+        queryset = super().get_queryset(request, *args, **kwargs)
+        tenant = tenant_from_request(request)
+        queryset = queryset.filter(tenant=tenant)
+        return queryset
+    
+    def save_model(self, request, obj, form, change):
+        tenant = tenant_from_request(request)
+        obj.tenant = tenant
+        super().save_model(request, obj, form, change)
+        
+
+class payloadAdmin(admin.ModelAdmin):
+    def get_queryset(self, request, *args, **kwargs):
+        queryset = super().get_queryset(request, *args, **kwargs)
+        tenant = tenant_from_request(request)
+        queryset = queryset.filter(tenant=tenant)
+        return queryset
+    
+    def save_model(self, request, obj, form, change):
+        tenant = tenant_from_request(request)
+        obj.tenant = tenant
+        super().save_model(request, obj, form, change)
+
+
+class launchAdmin(admin.ModelAdmin):
+    def get_queryset(self, request, *args, **kwargs):
+        queryset = super().get_queryset(request, *args, **kwargs)
+        tenant = tenant_from_request(request)
+        queryset = queryset.filter(tenant=tenant)
+        return queryset
+    
+    def save_model(self, request, obj, form, change):
+        tenant = tenant_from_request(request)
+        obj.tenant = tenant
+        super().save_model(request, obj, form, change)
+        
+
+admin.site.register(customer, customerAdmin)
+admin.site.register(rocket, rocketAdmin)
+admin.site.register(payload, payloadAdmin)
+admin.site.register(launch, launchAdmin)
 admin.site.register(Tenant)
