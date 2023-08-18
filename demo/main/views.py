7a5d2a80a006bf6569d4e3d86193eeeaad244892
Sajeyks <sajeyksmwangi@gmail.com>
2023-08-18 12:56:39 +0300

    feat: initial fix

--- demo/main/views.py
+++ demo/main/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
-
-from rest_framework import viewsets  # import 
+from rest_framework import viewsets
 from .models import customer, rocket, payload, launch
 from .serializers import customerSerializer, rocketSerializer, payloadSerializer, launchSerializer
+from demo.main.utils import tenant_from_request  
 # Create your views here.
 
 
@@ -10,17 +10,32 @@ class customerViewSet(viewsets.ModelViewSet):
     serializer_class = customerSerializer
     queryset = customer.objects.all()
     
+    def get_queryset(self):
+        tenant = tenant_from_request(self.request)
+        return super().get_queryset().filter(tenant=tenant)
     
 class rocketViewSet(viewsets.ModelViewSet):
     serializer_class = rocketSerializer
     queryset = rocket.objects.all()
+    
+    def get_queryset(self):
+        tenant = tenant_from_request(self.request)
+        return super().get_queryset().filter(tenant=tenant)
     
     
 class payloadViewSet(viewsets.ModelViewSet):
     serializer_class = payloadSerializer
     queryset = payload.objects.all()
     
+    def get_queryset(self):
+        tenant = tenant_from_request(self.request)
+        return super().get_queryset().filter(tenant=tenant)
+    
     
 class launchViewSet(viewsets.ModelViewSet):
     serializer_class = launchSerializer
     queryset = launch.objects.all()
+    
+    def get_queryset(self):
+        tenant = tenant_from_request(self.request)
+        return super().get_queryset().filter(tenant=tenant)
