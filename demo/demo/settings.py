d4125e739eff2dd9e9499a4a56ff397846f9035b
Sajeyks <sajeyksmwangi@gmail.com>
2023-08-23 12:39:42 +0300

    add the extention app

--- demo/demo/settings.py
+++ demo/demo/settings.py
@@ -40,8 +40,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework', # new
-    'main', # new
+    'django_extensions',
+    'rest_framework',
+    'main',
 ]
 
 MIDDLEWARE = [
