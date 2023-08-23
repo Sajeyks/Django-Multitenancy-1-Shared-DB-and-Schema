4b0a5d9444fa1e64253d2e3c7b40817b42afdbc4
Sajeyks <sajeyksmwangi@gmail.com>
2023-08-23 12:53:44 +0300

    remove the extention app

--- demo/demo/settings.py
+++ demo/demo/settings.py
@@ -40,9 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django_extensions',
-    'rest_framework',
-    'main',
+    'rest_framework', # new
+    'main', # new
 ]
 
 MIDDLEWARE = [
